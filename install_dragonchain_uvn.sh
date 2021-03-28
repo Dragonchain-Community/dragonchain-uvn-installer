962a07caf4c4c8383c3c1cd7841febe10f4820b6
l0wd0ct0r <johnathan.garrity@gmail.com>
2021-03-28 15:27:44 +0100

    Update install_dragonchain_uvn.sh

--- install_dragonchain_uvn.sh
+++ install_dragonchain_uvn.sh
@@ -299,12 +299,12 @@ bootstrap_environment() {
         sleep 2
         sudo ufw default allow routed >>$LOG_FILE 2>&1
         errchk $? "sudo ufw default allow routed >> $LOG_FILE 2>&1"
-        sleep 10
+        sleep 15
 
         sleep 2
         sudo ufw default allow outgoing >>$LOG_FILE 2>&1
         errchk $? "sudo ufw default allow outgoing >> $LOG_FILE 2>&1"
-        sleep 10
+        sleep 15
 
         sleep 2
         sudo ufw allow in on cni0 >>$LOG_FILE 2>&1 && sudo ufw allow out on cni0 >>$LOG_FILE 2>&1
