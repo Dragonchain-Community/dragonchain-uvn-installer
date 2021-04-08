cfff055199b0a6bd5191d7c631d3d17d052ed485
l0wd0ct0r <johnathan.garrity@gmail.com>
2021-04-08 12:15:41 +0100

    Update install_dragonchain_uvn.sh

--- install_dragonchain_uvn.sh
+++ install_dragonchain_uvn.sh
@@ -635,7 +635,7 @@ offer_apt_upgrade() {
 			errchk $? "sudo apt-get upgrade -y"
 		
 			# Reboot the system
-			echo -e "\n\e[93mIf there were upgrades to install listed above, we should now reboot the system.\e[0m"
+			echo -e "\n\e[93The operating system is now up-to-date, we should now reboot.\e[0m"
 		
 			local ANSWER=""
 			while [[ "$ANSWER" != "y" && "$ANSWER" != "yes" && "$ANSWER" != "n" && "$ANSWER" != "no" ]]; do
