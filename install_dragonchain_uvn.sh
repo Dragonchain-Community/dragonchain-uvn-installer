7e86b375b03c53f7f8e30ecc31684861d610ef78
l0wd0ct0r <johnathan.garrity@gmail.com>
2021-04-15 20:21:46 +0100

    Update install_dragonchain_uvn.sh

--- install_dragonchain_uvn.sh
+++ install_dragonchain_uvn.sh
@@ -587,7 +587,7 @@ check_matchmaking_status() {
 
         echo -e "\e[92mYOUR DRAGONCHAIN UVN '$DRAGONCHAIN_INSTALLER_DIR' IS ONLINE AND REGISTERED WITH THE MATCHMAKING API! HAPPY NODING!\e[0m"
         echo -e "\e[2mTo watch the status of this UVN, type 'sudo watch kubectl get pods -n $DRAGONCHAIN_INSTALLER_DIR'\e[0m"
-        echo -e "\e[2mTo watch the status of all UVNs, type 'sudo watch kubectl get pods --all-namespaces'\e[0m"
+        echo -e "\e[2mTo watch all UVNs, type 'sudo watch kubectl get pods --all-namespaces'\e[0m"
 
         #duck Prevent offering upgrade until latest kubernetes/helm issues are resolved
         #offer_apt_upgrade
